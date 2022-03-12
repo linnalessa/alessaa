8348eb20a0c0b613c00750d96cddfc14288fe15f
linnalessandra <linnalessandra@outlook.com>
2022-03-12 18:27:19 +0100

    react app

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+const Header = () => {
+    
+    return (
+<div>
+    <Container>
+    <h1>hello</h1>
+    </Container>
+</div>
+    );
+
+}
+
+export default Header;
